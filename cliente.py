36f975b20fb8024f6baa70b79a6977c19aa62f3a
vitorln10 <vitorln10@hotmail.com>
2019-09-22 21:03:32 -0300

    cliente estruturado em OO

--- cliente.py
+++ cliente.py
@@ -1,96 +1,123 @@
 #-*-confg: utf-8 -*-
+import tkinter as Tk
 import socket
 import time
-from tkinter import *
-import tkinter as Tk
+import threading
 
-log = Tk.Tk()
+class Conexao:
+    def __init__(self, master):
+        self.arq = open('./Servidor.txt', 'r')
+        self.texto = self.arq.read()
+        self.salvos = self.texto.split("\n")
+        self.arq.close()
+        
+        self.x1, self.x2 = self.salvos[0].split()
+        self.y1, self.y2 = self.salvos[1].split()
+        self.ip = Tk.StringVar()
+        self.ip.set(self.x1)
+        self.porta = Tk.StringVar()
+        self.porta.set(self.x2)
+        self.nick = Tk.StringVar()
+        self.nick.set(self.y1)
+        self.porta2 = Tk.StringVar()
+        self.porta2.set(self.y2)
 
-arq = open('./Servidor.txt', 'r')
-texto = arq.read()
+        self.master = master
+        self.master.wm_title("conexão com servidor")
+        self.master.wm_protocol('WM_DELETE_WINDOW', self.master.quit)
+        self.container = Tk.Frame(self.master)
+        self.container1 = Tk.Frame(self.container)
+        self.lbl1 = Tk.Label(self.container1, text="Endereco do servidor: ", anchor = Tk.W, width = 18)
+        self.ipText = Tk.Entry(self.container1, width=15, textvariable = self.ip)
+        self.container2 = Tk.Frame(self.container)
+        self.lbl2 = Tk.Label(self.container2, text="Porta do servidor: ", anchor = Tk.W, width = 18)
+        self.portaText = Tk.Entry(self.container2, width=15, textvariable = self.porta)
+        self.container3 = Tk.Frame(self.container)
+        self.lbl3 = Tk.Label(self.container3, text="Seu usuario: ", anchor = Tk.W, width = 18)
+        self.nickText = Tk.Entry(self.container3, width=15, textvariable = self.nick)
+        self.container4 = Tk.Frame(self.container)
+        self.lbl4 = Tk.Label(self.container4, text="Sua porta: ", anchor = Tk.W, width = 18)
+        self.porta2Text = Tk.Entry(self.container4,width=15, textvariable = self.porta2)
+        self.b_porta = Tk.Button(self.container, text="conectar", command=self.conecta ,bg="blue", fg="white")
 
-salvos = texto.split("\n")
-arq.close()
-x1, x2 = salvos[0].split()
-y1, y2 = salvos[1].split()
-
-ip = StringVar()
-ip.set(x1)
-porta = StringVar()
-porta.set(x2)
-nick = StringVar()
-nick.set(y1)
-porta2 = StringVar()
-porta2.set(y2)
-
-
-log.wm_title("conexão com servidor")
-log.wm_protocol('WM_DELETE_WINDOW', log.quit)
-container = Tk.Frame(log)
-container.pack(side = TOP, expand = 1, pady = 5, padx = 10)
-def conectando():
-    print("ok")
-    
-
-def conecta():
-    host = ipText.get()     # Endereco IP do Servidor
-    port = int(portaText.get())  # Porta que o Servidor esta
-    usuario = nickText.get()
-    minhaPorta = porta2Text.get()
-    arq = open('./Servidor.txt', 'w')
-    texto = host + " " + str(port) + "\n" + usuario + " " + minhaPorta
-    arq.write(texto)
-    arq.close()
-    log.withdraw()
-    conexao1 = Tk.Tk()
-    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    dest = (host, port)
-
-    conexao = "USER " + usuario + " " + minhaPorta
-    print(conexao)
-    while (True):
-        udp.sendto(bytes(conexao, encoding='utf8'), dest) 
-        time.sleep(10)
-    udp.close()
+        self.container.pack(side = Tk.TOP, expand = 1, pady = 5, padx = 10)        
+        self.container1.pack()
+        self.lbl1.pack(side = Tk.LEFT, padx = 8, pady=5)
+        self.ipText.pack(side = Tk.RIGHT)
+        self.container2.pack()
+        self.lbl2.pack(side = Tk.LEFT, padx = 8, pady=5)
+        self.portaText.pack(side = Tk.RIGHT)
+        self.container3.pack()
+        self.lbl3.pack(side = Tk.LEFT, padx = 8, pady=5)
+        self.nickText.pack(side = Tk.RIGHT)
+        self.container4.pack()
+        self.lbl4.pack(side = Tk.LEFT, padx = 8, pady=5)
+        self.porta2Text.pack(side = Tk.RIGHT)
+        self.b_porta.pack(padx = 8, pady=5)
 
 
-container1 = Tk.Frame(container)
-container1.pack()
 
-lbl1 = Label(container1, text="Endereco do servidor: ", anchor = W, width = 18)
-lbl1.pack(side = LEFT, padx = 8, pady=5)
+    def conecta(self):
+        host = self.ipText.get()     # Endereco IP do Servidor
+        port = int(self.portaText.get())  # Porta que o Servidor esta
+        usuario = self.nickText.get()
+        minhaPorta = self.porta2Text.get()
+        arq = open('./Servidor.txt', 'w')
+        texto = host + " " + str(port) + "\n" + usuario + " " + minhaPorta
+        arq.write(texto)
+        arq.close()
+        self.master.withdraw()
+        self.newWindow = Tk.Toplevel(self.master)
+        self.app = selecao(self.newWindow, self.master, host, port, usuario, minhaPorta)
+        
 
-ipText = Entry(container1, width=15, textvariable = ip)
-ipText.pack(side = RIGHT)
+class selecao:
+    def __init__(self, master, conexaoMaster, host, port, usuario, minhaPorta):
+        self.udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.dest = (host, port)
 
-container2 = Tk.Frame(container)
-container2.pack()
+        self.master = master
+        self.conexaoMaster = conexaoMaster
 
-lbl2 = Label(container2, text="Porta do servidor: ", anchor = W, width = 18)
-lbl2.pack(side = LEFT, padx = 8, pady=5)
+        self.master.wm_title("conexão com servidor")
+        self.master.wm_protocol('WM_DELETE_WINDOW', self.master.quit)
+        self.container = Tk.Frame(self.master)
+        self.lista = Tk.Listbox(self.container)
+        self.container2 = Tk.Frame(self.container)
+        self.b_porta = Tk.Button(self.container2, text="partida selecionada" ,bg="blue", fg="white", width=15)
+        self.b_porta1 = Tk.Button(self.container2, text="partida aleatoria" ,bg="blue", fg="white", width=15)
+        self.b_porta1 = Tk.Button(self.container2, text="partida aleatoria" ,bg="blue", fg="white", width=15)
+        self.espaco = Tk.LabelFrame(self.container2, height = 50)
+        self.b_porta2 = Tk.Button(self.container2, text="Sair", command=self.close_windows, bg="red", fg="white", width=15)
 
-portaText = Entry(container2,width=15, textvariable = porta)
-portaText.pack(side = RIGHT)
+        self.container.pack(side = Tk.TOP, expand = 1, pady = 5, padx = 10)
+        self.lista.pack(side = Tk.LEFT, padx = 8, pady=5)
+        self.container2.pack(side = Tk.LEFT)
+        self.b_porta.pack(padx = 8, pady=5)
+        self.b_porta1.pack(padx = 8, pady=5)
+        self.espaco.pack()
+        self.b_porta2.pack(side = Tk.BOTTOM, padx = 8, pady = 5)
+        
+        t = threading.Thread(target=self.userLoop, args=(usuario, minhaPorta))
+        t.daemon = True
+        t.start()
 
-container3 = Tk.Frame(container)
-container3.pack()
+    def userLoop(self, usuario, minhaPorta): 
+        conexao = "USER " + usuario + " " + minhaPorta
+        print(conexao)
+        while (True):
+            self.udp.sendto(bytes(conexao, encoding='utf8'), self.dest) 
+            time.sleep(10)
+        self.udp.close()
+        
+    def close_windows(self):
+        #self.conexaoMaster.deiconify()
+        self.conexaoMaster.destroy()
 
-lbl3 = Label(container3, text="Seu usuario: ", anchor = W, width = 18)
-lbl3.pack(side = LEFT, padx = 8, pady=5)
+def main(): 
+    root = Tk.Tk()
+    app = Conexao(root)
+    root.mainloop()
 
-nickText = Entry(container3, width=15, textvariable = nick)
-nickText.pack(side = RIGHT)
-
-container4 = Tk.Frame(container)
-container4.pack()
-
-lbl4 = Label(container4, text="Sua porta: ", anchor = W, width = 18)
-lbl4.pack(side = LEFT, padx = 8, pady=5)
-
-porta2Text = Entry(container4,width=15, textvariable = porta2)
-porta2Text.pack(side = RIGHT)
-
-b_porta = Button(container, text="conectar", command=conecta ,bg="blue", fg="white")
-b_porta.pack(padx = 8, pady=5)
-
-Tk.mainloop()
+if __name__ == '__main__':
+    main()
